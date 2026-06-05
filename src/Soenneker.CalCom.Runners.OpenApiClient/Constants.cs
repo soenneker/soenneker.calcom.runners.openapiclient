44853aa672d1c692465082db1ea2c757c8f650a5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:59 -0500

    xml commenting

--- src/Soenneker.CalCom.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.CalCom.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.CalCom.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.CalCom.OpenApiClient";
 }
